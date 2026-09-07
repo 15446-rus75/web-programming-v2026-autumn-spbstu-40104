839bed80ffb52b197ca079f04ae8a7a17d4756c9
mkryloff <matwej71@gmail.com>
2026-09-07 21:40:13 +0300

    Event class added with methods

--- krylov.matvey/lab4/model.js
+++ krylov.matvey/lab4/model.js
@@ -1,2 +1,23 @@
-// Экспортируйте класс и функции коллекций с именами из контракта вашего варианта.
-export {};
+export class Event {
+  constructor(id, title, participants, date) {
+    this.id = id;
+    this.title = title;
+    this.participants = participants;
+    this.date = date;
+  }
+
+  addParticipant(name) {
+    this.participants.push(name);
+  }
+
+  removeParticipant(name) {
+    const index = this.participants.indexOf(name)
+    if (index >= 0) {
+      this.participants.splice(index, 1);
+    }
+  }
+
+  get participantCount() {
+    return this.participants.length;
+  }
+};
